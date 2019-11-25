425bdc360626fac84744575be2fb61a53e813085
Chi-chi Ekweozor <thisischichi@gmail.com>
2019-11-25 12:06:03 +0000

    Update Display from number keys

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Calculator from './Calculator';
 import Display from '../Display/Display';
 import Keypad from '../Keypad/Keypad';
@@ -11,7 +11,7 @@ describe('Calculator', () => {
   it('should render correctly', () => expect(wrapper).toMatchSnapshot());
 
   it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').length).toEqual(2);
   });
 
   it('should render the Display Component', () => {
@@ -31,5 +31,81 @@ describe('Calculator', () => {
         updateDisplay={wrapper.instance().updateDisplay}
       />
     ])).toEqual(true);
+  });
+});
+
+describe('mounted Calculator', () => {
+  let wrapper;
+  beforeEach(() => wrapper = mount(<Calculator />));
+
+  it('calls updateDisplay when a number key is clicked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'updateDisplay');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.number-key').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls setOperator when an operator key is clicked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'setOperator');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.operator-key').first().simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls callOperator when the submit key is clicked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'callOperator');
+    wrapper.instance().forceUpdate();
+    expect(spy).toHaveBeenCalledTimes(0);
+    wrapper.find('.submit-key').simulate('click');
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('updateDisplay', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<Calculator />));
+
+  it('updates displayValue', () => {
+    wrapper.instance().updateDisplay('5');
+    expect(wrapper.state('displayValue')).toEqual('5');
+  });
+
+  it('concatenates displayValue', () => {
+    wrapper.instance().updateDisplay('5');
+    wrapper.instance().updateDisplay('0');
+    expect(wrapper.state('displayValue')).toEqual('50');
+  });
+
+  it('removes leading "0" from displayValue', () => {
+    wrapper.instance().updateDisplay('0');
+    expect(wrapper.state('displayValue')).toEqual('0');
+    wrapper.instance().updateDisplay('5');
+    expect(wrapper.state('displayValue')).toEqual('5');
+  });
+
+  it('prevents multiple leading "0"s from displayValue', () => {
+    wrapper.instance().updateDisplay('0');
+    wrapper.instance().updateDisplay('0');
+    expect(wrapper.state('displayValue')).toEqual('0');
+  });
+
+  it('removes last char of displayValue', () => {
+    wrapper.instance().updateDisplay('5'); 
+    wrapper.instance().updateDisplay('0'); 
+    wrapper.instance().updateDisplay('ce'); 
+    expect(wrapper.state('displayValue')).toEqual('5');
+  });
+
+  it('prevents multiple instances of "." in displayValue', () => {
+    wrapper.instance().updateDisplay('.'); 
+    wrapper.instance().updateDisplay('.'); 
+    expect(wrapper.state('displayValue')).toEqual('.');
+  });
+
+  it('will set displayValue to "0" if displayValue is equal to an empty string', () => {
+    wrapper.instance().updateDisplay('ce'); 
+    expect(wrapper.state('displayValue')).toEqual('0');
   });
 });
